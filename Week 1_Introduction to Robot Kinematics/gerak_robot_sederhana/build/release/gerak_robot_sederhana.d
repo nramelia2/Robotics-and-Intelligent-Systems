676f176b06b1561b695d265a7a9fbdf98e9b6dac
Nurul Amelia <nurulamelia638@gmail.com>
2022-12-01 18:14:02 +0700

    Robotics and Intelligent Systems

---
+++ Week 1_Introduction to Robot Kinematics/gerak_robot_sederhana/build/release/gerak_robot_sederhana.d
@@ -0,0 +1,5 @@
+build/release/gerak_robot_sederhana.o: gerak_robot_sederhana.c \
+ C:\Program\ Files\Webots/include/controller/c/webots/robot.h \
+ C:\Program\ Files\Webots/include/controller/c/webots/types.h \
+ C:\Program\ Files\Webots/include/controller/c/webots/nodes.h \
+ C:\Program\ Files\Webots/include/controller/c/webots/motor.h
