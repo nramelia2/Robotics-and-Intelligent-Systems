78b2f14822af7f3c5f45fc849025e65ec9fea194
Nurul Amelia <nurulamelia638@gmail.com>
2022-12-04 16:03:07 +0700

    Robotics and Intelligent System

---
+++ Week 7_Robotic Sensors and Introduction to Computer Vision/gerak_robot_sederhana/build/release/gerak_robot_sederhana.d
@@ -0,0 +1,6 @@
+build/release/gerak_robot_sederhana.o: gerak_robot_sederhana.c \
+ C:\Program\ Files\Webots/include/controller/c/webots/robot.h \
+ C:\Program\ Files\Webots/include/controller/c/webots/types.h \
+ C:\Program\ Files\Webots/include/controller/c/webots/nodes.h \
+ C:\Program\ Files\Webots/include/controller/c/webots/motor.h \
+ C:\Program\ Files\Webots/include/controller/c/webots/distance_sensor.h
